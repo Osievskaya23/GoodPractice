06f7e9befdf96638b0f8bfeb1d13f2c043cbb8d9
Valentina Osievskaya <valentinaosievskaya@gmail.com>
2019-01-29 16:23:40 +0200

    Overwriting the Object Class clone() method

--- immutableclass/ImmutableClassTest.java
+++ immutableclass/ImmutableClassTest.java
@@ -1,7 +1,7 @@
 package immutableclass;
 
 public class ImmutableClassTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException{
         ImmutableAnimal cat = new ImmutableAnimal("Cat", 3, new Family("father Cat", "mother Cat"));
         System.out.println("Cat before changing family:" +
                 "cat name = " + cat.getName() + ";\n" +
@@ -15,60 +15,5 @@ public class ImmutableClassTest {
                 "cat age = " + cat.getAge() + ";\n" +
                 "cat's father = " + cat.getFamily().getFather() + ";\n" +
                 "cat's mother = " + cat.getFamily().getMother() + ";");
-    }
-}
-
-final class ImmutableAnimal {
-
-    private final String name;
-    private final int age;
-    private final Family family;
-
-    ImmutableAnimal(String name, int age, Family family) {
-        this.name = name;
-        this.age = age;
-        //this.family = family;
-        Family familyClone = new Family(family.getFather(), family.getMother());
-        this.family = familyClone;
-    }
-
-    String getName() {
-        return name;
-    }
-
-    int getAge() {
-        return age;
-    }
-
-    Family getFamily() {
-        //return family;
-        return new Family(this.family.getFather(), this.family.getMother());
-    }
-}
-
-class Family {
-
-    private String father;
-    private String mother;
-
-    Family(String father, String mother) {
-        this.father = father;
-        this.mother = mother;
-    }
-
-    String getFather() {
-        return father;
-    }
-
-    void setFather(String father) {
-        this.father = father;
-    }
-
-    String getMother() {
-        return mother;
-    }
-
-    void setMother(String mother) {
-        this.mother = mother;
     }
 }
