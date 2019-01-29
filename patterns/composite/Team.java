e983f508237a810a7219f090f7a1b43e0877708d
Valentina Osievskaya <valentinaosievskaya@gmail.com>
2019-01-29 17:22:56 +0200

    add modifiers

--- patterns/composite/Team.java
+++ patterns/composite/Team.java
@@ -3,18 +3,18 @@ package patterns.composite;
 import java.util.ArrayList;
 import java.util.List;
 
-class Team {
+public class Team {
     private List<Developer> developers = new ArrayList<>();
 
-    void addDeveloper(Developer developer) {
+    public void addDeveloper(Developer developer) {
         developers.add(developer);
     }
 
-    void removeDeveloper(Developer developer) {
+    public void removeDeveloper(Developer developer) {
         developers.remove(developer);
     }
 
-    void createProject() {
+    public void createProject() {
         System.out.println("Team creates project...\n");
         for(Developer developer : developers) {
             developer.writeCode();
