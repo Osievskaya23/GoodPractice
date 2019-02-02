6f7c7564beb0054dc69afb12a7521aa6b673fe8d
Osievskaya23 <36424928+Osievskaya23@users.noreply.github.com>
2019-02-02 22:48:47 +0200

    Delete JavaDeveloper.java

--- patterns/composite/JavaDeveloper.java
+++
@@ -1,8 +0,0 @@
-package patterns.composite;
-
-public class JavaDeveloper implements Developer {
-    @Override
-    public void writeCode() {
-        System.out.println("Java developer writes Java code...");
-    }
-}
