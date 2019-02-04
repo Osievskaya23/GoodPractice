0321bdc1249012a9b5b8c175497a5e66be008ac6
Osievskaya23 <36424928+Osievskaya23@users.noreply.github.com>
2019-02-04 17:24:30 +0200

    Update JavaDeveloper.java

--- com/patterns/composite/JavaDeveloper.java
+++ com/patterns/composite/JavaDeveloper.java
@@ -1,6 +1,6 @@
 package patterns.composite;
 
-class JavaDeveloper implements Developer {
+public class JavaDeveloper implements Developer {
     @Override
     public void writeCode() {
         System.out.println("Java developer writes Java code...");
